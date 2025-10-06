6c117f814874402bb1af00c2e2beca59aefe35cd
ViniciusGR797 <loritoamarelo@outlook.com>
2025-10-06 03:44:03 -0300

    fix: lint adjustment

--- app/main.py
+++ app/main.py
@@ -5,14 +5,24 @@ from controllers.messages import message_handler
 def handler(event, context):
     return message_handler(event)
 
+
 if __name__ == "__main__":
     event = {
         "Records": [
             {
                 "messageId": "123",
                 "receiptHandle": "abc",
-                "body": json.dumps({"message_id": "1", "payload": {}, "timestamp": "2025-10-06T00:00:00", "source": "local", "type": "test"}),
-                "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:main_queue.fifo"
+                "body": json.dumps(
+                    {
+                        "message_id": "1",
+                        "payload": {},
+                        "timestamp": "2025-10-06T00:00:00",
+                        "source": "local",
+                        "type": "test"
+                    }
+                ),
+                "eventSourceARN":
+                    "arn:aws:sqs:us-east-1:123456789012:main_queue.fifo"
             }
         ]
     }
